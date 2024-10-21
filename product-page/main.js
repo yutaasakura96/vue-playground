7225f69f2bf5df76b88e99664ed9f8b4aa530334
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-21 12:17:12 +0900

    feat: enhance product page with image and external link
    - Updated 'index.html' to include a product display section with image and description.
    - Wrapped product image in a clickable link directing to Vue Mastery.
    - Added dynamic binding for the product image source using Vue's 'v-bind'.
    - Modified 'main.js' to include the product image and URL for external navigation.

--- product-page/main.js
+++ product-page/main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
   data() {
-      return {
-          product: 'Socks',
-          description: 'A warm fuzzy pair of socks.'
-      }
-  }
-})
+    return {
+      product: "Socks",
+      description: "A warm fuzzy pair of socks.",
+      image: "./assets/images/socks_blue.jpg",
+      url: "https://www.vuemastery.com/",
+    };
+  },
+});
