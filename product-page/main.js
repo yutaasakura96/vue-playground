ca2291d9cf8da59f84409d11e214e415ff33bdbb
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-21 17:55:50 +0900

    feat: add product details, sizes, and available colors to product page
    - Updated 'index.html' to display product materials, sizes, and available color options.
      - Added an unordered list for displaying materials under 'Materials'.
      - Included a dynamic list for product sizes and colors.
    - Modified 'main.js' to include new data properties for 'details', 'sizes', and 'variants' (colors).

--- product-page/main.js
+++ product-page/main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       url: "https://www.vuemastery.com/",
       inventory: 100,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
