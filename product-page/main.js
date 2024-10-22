54b2f6ffcb1657316df7dbb02b5c4f19a8c29651
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-22 18:28:33 +0900

    feat: add cart functionality, dynamic product details, and stock status to product page
    - Added a shopping cart feature with 'Add to Cart' and 'Remove Item' buttons, along with dynamic stock handling.
    - Implemented hover functionality to update product image and stock status based on the selected color variant.
    - Refactored product title to include the brand name and computed properties for image, inStock, and sale status.
    - Updated 'main.js' with methods for cart manipulation and variant switching.
    - Modified 'index.html' to dynamically display stock availability, product details, and sales status.
    - Adjusted styles in 'styles.css' to reflect new functionality and added cursor pointer for interactive elements.

--- product-page/main.js
+++ product-page/main.js
@@ -1,18 +1,59 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       description: "A warm fuzzy pair of socks.",
-      image: "./assets/images/socks_blue.jpg",
       url: "https://www.vuemastery.com/",
-      inventory: 100,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      selectedVariant: 0,
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
       sizes: ["S", "M", "L", "XL"],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " is on sale!";
+      }
+      return "";
+    },
   },
 });
